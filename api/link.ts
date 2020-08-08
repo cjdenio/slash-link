385f65e473d284a5eada417aaff3e37177eb10b2
Caleb Denio <cjdenio44@gmail.com>
2020-08-07 20:14:12 -0400

    Made it work

--- api/link.ts
+++ api/link.ts
@@ -1,12 +1,21 @@
 import { NowRequest, NowResponse } from "@vercel/node";
-import axios from "axios";
+import axios, { AxiosResponse } from "axios";
 
-export default (req: NowRequest, res: NowResponse) => {
-  axios("https://u.nu/api.php", {
-    params: {
-      action: "shorturl",
-      format: "json",
-      url: req.body.text.trim(),
-    },
-  });
+export default async (req: NowRequest, res: NowResponse) => {
+  let resp: AxiosResponse;
+  try {
+    resp = await axios("https://u.nu/api.php", {
+      params: {
+        action: "shorturl",
+        format: "json",
+        url: req.body.text.trim(),
+      },
+    });
+
+    res.send(
+      `Hooray! :tada: Here's your shortened link: ${resp.data.shorturl}`
+    );
+  } catch (e) {
+    res.send("Something went wrong :cry:");
+  }
 };
