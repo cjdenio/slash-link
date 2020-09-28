4f0d832ccf4cd321c773cfb01b3b08fc51172b86
Caleb Denio <cjdenio44@gmail.com>
2020-09-28 13:35:54 -0400

    Important stuff

--- api/link.ts
+++ api/link.ts
@@ -2,20 +2,25 @@ import { NowRequest, NowResponse } from "@vercel/node";
 import axios, { AxiosResponse } from "axios";
 
 export default async (req: NowRequest, res: NowResponse) => {
-  let resp: AxiosResponse;
-  try {
-    resp = await axios("https://u.nu/api.php", {
-      params: {
-        action: "shorturl",
-        format: "json",
-        url: req.body.text.trim(),
-      },
-    });
+  // let resp: AxiosResponse;
+  // try {
+  //   resp = await axios("https://u.nu/api.php", {
+  //     params: {
+  //       action: "shorturl",
+  //       format: "json",
+  //       url: req.body.text.trim(),
+  //     },
+  //   });
 
-    res.send(
-      `Hooray! :tada: Here's your shortened link: ${resp.data.shorturl}`
-    );
-  } catch (e) {
-    res.send("Something went wrong :cry:");
+  //   res.send(
+  //     `Hooray! :tada: Here's your shortened link: ${resp.data.shorturl}`
+  //   );
+  // } catch (e) {
+  //   res.send("Something went wrong :cry:");
+  // }
+  if (req.body.text == "") {
+    res.send("Please run this command like so: `/link <url>`");
+    return;
   }
+  res.send("Hooray! :tada: Here's your shortened link: https://u.nu/0lzf-");
 };
